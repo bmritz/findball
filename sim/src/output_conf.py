30a6a1c4ead17936b41c1dab870c20cf39eb4ec3
Brian Ritz <brian.ritz@aunalytics.com>
2016-10-30 11:01:17 -0400

    added absolute path and group to delete_if_exists

--- sim/src/output_conf.py
+++ sim/src/output_conf.py
@@ -1,14 +1,24 @@
-import h5py
-import logging
+import h5py, logging, os
 
-HDF5_NAME = "DATA.hdf5"
+DATA_FILE = "../data/DATA.hdf5"
+
+HDF5_NAME = os.path.abspath(DATA_FILE)
 
 DATA = h5py.File(HDF5_NAME)
 
-def delete_if_exists(ds_name):
-    try:
-        del DATA[ds_name]
-    except KeyError:
-        pass
+def delete_if_exists(ds_name, group=None):
+    if group is None:
+        try:
+            del DATA[ds_name]
+        except KeyError:
+            pass
+        else:
+            logging.info("Deleted dataset %s from %s because it will be replaced by this run." % (ds_name, HDF5_NAME))
     else:
-        logging.info("Deleted dataset %s from %s because it will be replaced by this run." % (ds_name, HDF5_NAME))
+        grp = DATA[group]
+        try: 
+            del grp[ds_name]
+        except KeyError:
+            pass
+        else:
+            logging.info("Deleted dataset %s from h5file: %s, group: %s because it will be replaced by this run." % (ds_name, HDF5_NAME, group))
